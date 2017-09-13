e3df0ffb2294f07306c55345f7be6690c28014b5
Martin Sollenberg <martin.sollenberg@live.se>
2017-09-13 10:21:55 +0200

    Added blacklist column in script, not working yet

--- schema.sh
+++ schema.sh
@@ -1,11 +1,7 @@
 #!bash
 
-mysql -u webadmin --password=adminadmin
-USE webshop;
+mysql -u webadmin --password=adminadmin -e "USE webshop;
+drop table users;
 CREATE TABLE users (name VARCHAR(20) CHARACTER SET utf8,
-                    password VARCHAR(200), salt VARCHAR(50));
-
-
-
-
-
+                    password VARCHAR(200), salt VARCHAR(50), blacklist VARCHAR(50));
+                    "
