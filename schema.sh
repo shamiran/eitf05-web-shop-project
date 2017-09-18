dfb6a0ceeb7790f0359e0c10837c005db2f94d1c
Martin Sollenberg <martin.sollenberg@live.se>
2017-09-18 09:42:18 +0200

    played with the layout and css of the index page, register new user is not fixed

--- schema.sh
+++ schema.sh
@@ -2,7 +2,9 @@
 
 mysql -u webadmin --password=adminadmin -e "
   USE webshop;
-  CREATE TABLE users (name VARCHAR(20) CHARACTER SET utf8,
+  drop table users;
+  drop table blacklist;
+  CREATE TABLE users (username VARCHAR(20) CHARACTER SET utf8,
                       password VARCHAR(200),
                       salt VARCHAR(50));
   CREATE TABLE blacklist (password VARCHAR(200));"
