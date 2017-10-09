1f678f9895d83a16e9a9b1be3bc5db359272020f
Martin Sollenberg <martin.sollenberg@live.se>
2017-10-09 14:34:42 +0200

    Small changes in database init script
    changed the price column in the products table to be an int and also changes the initial blacklist passwords

--- schema.sh
+++ schema.sh
@@ -17,7 +17,7 @@ mysql -u webadmin --password=adminadmin -e "
   CREATE TABLE blacklist (password VARCHAR(200));
 	CREATE TABLE products (id int AUTO_INCREMENT NOT NULL,
 			                   name VARCHAR(50),
-                         price VARCHAR(10),
+                         price INT,
                          image VARCHAR(200),
                          PRIMARY KEY (id));
 
@@ -26,16 +26,14 @@ mysql -u webadmin --password=adminadmin -e "
   INSERT INTO blacklist (password)
     VALUES
     ('Abcd1234'),
-    ('12345678'),
-    ('11111111'),
-    ('11223344'),
-    ('123456789'),
-    ('987654321');
+    ('Hello123'),
+    ('Cat12345'),
+    ('ABCD123');
 
     INSERT INTO products (id,name,price,image)
       VALUES
-      (1, 'Iphone X', '1000.00','http://drop.ndtv.com/TECH/product_database/images/913201720152AM_635_iphone_x.jpeg' ),
-      (2, 'Samsung Galaxy S8', '800.00', 'http://drop.ndtv.com/TECH/product_database/images/329201783846PM_635_samsung_galaxy_s8.jpeg'),
-      (3, 'Sony Xperia XZ', '800.00', 'http://cdn2.gsmarena.com/vv/pics/sony/sony-xperia-xz-premium-2017-0.jpg'),
-      (4, 'Motorola Moto Z2', '700.00', 'http://cdn2.gsmarena.com/vv/pics/motorola/motorola-moto-z2-play-0.jpg') ;
+      (1, 'Iphone X', 1000,'http://drop.ndtv.com/TECH/product_database/images/913201720152AM_635_iphone_x.jpeg' ),
+      (2, 'Samsung Galaxy S8', 800, 'http://drop.ndtv.com/TECH/product_database/images/329201783846PM_635_samsung_galaxy_s8.jpeg'),
+      (3, 'Sony Xperia XZ', 800, 'http://cdn2.gsmarena.com/vv/pics/sony/sony-xperia-xz-premium-2017-0.jpg'),
+      (4, 'Motorola Moto Z2', 700, 'http://cdn2.gsmarena.com/vv/pics/motorola/motorola-moto-z2-play-0.jpg') ;
     "
