13602662367640dc1fe57a64af78034a59dccb88
Johnny <johnnyponny93@gmail.com>
2017-10-09 02:37:55 +0200

    Change to prepared statements in login.php

--- login.php
+++ login.php
@@ -12,31 +12,41 @@
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
+	
+	$stmt = $conn->prepare("SELECT * FROM users WHERE (username) LIKE (?)");
+	$stmt2 = $conn->prepare("UPDATE users SET lastLoginAttempt = '" . date("Y-m-d H:i:s") . "' WHERE (username) LIKE (?)");
+	$stmt3 = $conn->prepare("UPDATE users SET loginAttemptCount = (?)  WHERE (username) LIKE (?) ");
+	$username = $_REQUEST['username'];
+	$password = mysqli_real_escape_string($conn,$_REQUEST['password']);
 
-	//strip tags to remove if trying to insert html tags
-	//trim to remove whitespace
-	//htmlspecialchars????
-	$username = mysqli_real_escape_string($conn,$_REQUEST['username']));
-	$password = mysqli_real_escape_string($conn,$_REQUEST['password']));
-
-$sql = "SELECT * FROM users WHERE username LIKE '" . $username . "'";
-
-$result = $conn->query($sql);
+	if (!$stmt) {
+		echo "false";
+	}
+	else {
+	$stmt->bind_param("s",$username);
+	$stmt->execute();
+	}
+	$result = $stmt->get_result();
 
 if($result->num_rows == 0){
 	$login = false;
 	$reason = 2;
 } else {
 	$row = $result->fetch_assoc();
-
-	$sql = "UPDATE users SET lastLoginAttempt = '" . date("Y-m-d H:i:s") . "' WHERE username LIKE '" . $username . "'";
-	$conn->query($sql);
+	if (!$stmt2) {
+		echo "false";
+	}
+	else {
+		$stmt2->bind_param("s",$username);
+		$stmt2->execute();
+	}
 
 	$mysql_date_now = new DateTime("now");
 	$date_lastLogin = new DateTime($row['lastLoginAttempt']);
 
 	if($mysql_date_now->getTimestamp() - $date_lastLogin->getTimestamp() >= 60 || $row['loginAttemptCount']<5-1){
 		$salt = $row['salt'];
+		$lac = $row['loginAttemptCount'];
 		if(md5($password.$salt) === $row['password']){
 			$token = makeMeAToken(40);
 			$login = true;
@@ -44,19 +54,25 @@ if($result->num_rows == 0){
 			$_SESSION["csrftoken"] = $token;
 			$_SESSION["username"] = $username;
 			//$_SESSION["num_products"] = 0;
-			$sql = "UPDATE users SET loginAttemptCount = 1 WHERE username LIKE '".$username . "'";
-			$conn->query($sql);
+			if (!$stmt3) {
+				echo "false";
+			}
+			else {
+				$stmt3->bind_param("is",$lac, $username);
+				$lac = 1;
+				$stmt3->execute();
+			}
 		} else {
 			$login = false;
 			$reason = 1;
 
 			if($mysql_date_now->getTimestamp() - $date_lastLogin->getTimestamp() < 60){
-				$sql = "UPDATE users SET loginAttemptCount = loginAttemptCount + 1 WHERE username LIKE '".$username . "'";
-				$conn->query($sql);
+				$lac++;
 			} else {
-				$sql = "UPDATE users SET loginAttemptCount = 1 WHERE username LIKE '".$username . "'";
-				$conn->query($sql);
+				$lac = 1;
 			}
+				$stmt3->bind_param("is",$lac, $username);
+				$stmt3->execute();
 		}
 	} else {
 		$login = false;
@@ -115,6 +131,10 @@ function makeMeAToken($max=40){
 				<?php
 					if($login){
 						echo 'User found. Password correct';
+						$stmt->close();
+						$stmt2->close();
+						$stmt3->close();
+						$conn->close();
 					} else {
 						if($reason==0){
 							echo 'Too many recent login attempts, please wait 60 seconds';
